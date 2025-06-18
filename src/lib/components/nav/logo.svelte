753ee88ba6fc59c8713eb9db1b35392abca4296c
Lakelimbo <gabrielmarcopolo@gmail.com>
2025-06-18 15:06:53 -0300

    static shenanigans

--- src/lib/components/nav/logo.svelte
+++ src/lib/components/nav/logo.svelte
@@ -1,8 +1,8 @@
-<a href="/" class="flex items-center gap-2">
-	<img
-		src="/logo.svg"
-		alt="Bulbagarden"
-		width={35}
-	/>
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<a href="/{base}" class="flex items-center gap-2">
+	<img src="{base}/logo.svg" alt="Bulbagarden" width={35} />
 	<span class="font-serif text-2xl font-black">Bulbagarden</span>
 </a>
